d765f29fa2d81dba682d9f12f6009013ab5e5aad
Sandeep55k <Sandeep55k@gmail.com>
2023-10-03 08:20:00 +0530

    Adding menu class

---
+++ PROG5001ASSIGNMENT2/Menu.java
@@ -0,0 +1,25 @@
+
+/**
+ * Assignment2 - PROG5001
+ *
+ * @author Sandeep Kaur / 24066442
+ * 
+ */
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Menu
+{
+    // instance variables list of students
+    private List<Student> list;
+
+    /**
+     * Constructor for objects of class Menu
+     */
+    public Menu(List<Student> list)
+    {
+        // initialise instance variables
+        this.list = list;
+    }
+}
